c52c882e6e58471dc02e44376f4910b428c1799f
Edgard <edigarhenriqu@gmail.com>
2025-02-11 11:06:33 -0300

    Sequenciador de senhas

---
+++ src/screens/Home/Home.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import {Text, View} from "react-native";
+import { StatusBar } from 'expo-status-bar';
+import styles from './Style';
+import { Menu } from "../../components/Menu/Menu";
+
+export default function Home () {
+    return(
+        <View style={styles.container}>
+        <Menu/>
+        <Text>Open up App.tsx to start working on your app!</Text>
+        <StatusBar style="auto" />
+          </View>
+    )
+}
